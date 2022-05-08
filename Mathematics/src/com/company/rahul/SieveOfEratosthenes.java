86e32025ba7168bf99ed7acfb927112b0674c97e
Rahul_Anand72 <rahul.anand88096@gmail.com>
2022-05-08 21:18:01 +0530

    Less optimised code has been added.

--- Mathematics/src/com/company/rahul/SieveOfEratosthenes.java
+++ Mathematics/src/com/company/rahul/SieveOfEratosthenes.java
@@ -1,5 +1,7 @@
 package com.company.rahul;
 
+import java.util.Arrays;
+
 public class SieveOfEratosthenes {
     public static void main(String[] args){
         int n = 20;
@@ -8,9 +10,20 @@ public class SieveOfEratosthenes {
     }
 
     static void getPrime(int n) {
-        for ( int i = 2 ; i <=n ; i++ ) {
-            if(PrimeNumber.getSignal(i))
+        boolean[] arr = new boolean[n+1];
+        Arrays.fill(arr, true);
+
+        for ( int i = 2 ; i*i <=n ; i++ ) {
+            if(arr[i]) {
+                for ( int j = 2*i ; j <= n ; j = j+i ) {
+                    arr[j] = false;
+                }
+            }
+        }
+        for ( int i = 2 ; i <= n ; i++ ) {
+            if(arr[i])
                 System.out.println(i);
+            
         }
     }
 }
