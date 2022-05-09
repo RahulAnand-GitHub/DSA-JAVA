2955d12c61ada6abf76da18c4b4a99ab7bb0809a
Rahul_Anand72 <rahul.anand88096@gmail.com>
2022-05-09 16:56:21 +0530

    More efficient solution has been added.

--- Mathematics/src/com/company/rahul/Power.java
+++ Mathematics/src/com/company/rahul/Power.java
@@ -3,15 +3,21 @@ package com.company.rahul;
 public class Power {
     public static void main(String[] args){
         int n = 2;
-        int p = 6;
+        int p = 7;
         System.out.println(getPower(n, p));
 
     }
 
     public static int getPower(int n, int p) {
-        int result = 1;
-        for ( int i = 0 ; i <p ; i++ )
-            result *=n;
-        return result;
+//        int result = 1;
+//        for ( int i = 0 ; i <p ; i++ )
+//            result *=n;
+//        return result;
+        if(p==0)
+            return 1;
+        int temp = getPower(n, p/2);
+        temp = temp *  temp;
+        if(p%2==0)  return temp;
+        else    return temp * n;
     }
 }
