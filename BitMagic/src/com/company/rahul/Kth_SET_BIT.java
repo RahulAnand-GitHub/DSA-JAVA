0f563e6a7ddb792a88e1bb491d19e2a39c884e15
Rahul_Anand72 <rahul.anand88096@gmail.com>
2022-05-15 18:16:40 +0530

    2nd approach has been added to Kth position set bit program.

--- BitMagic/src/com/company/rahul/Kth_SET_BIT.java
+++ BitMagic/src/com/company/rahul/Kth_SET_BIT.java
@@ -2,8 +2,14 @@ package com.company.rahul;
 
 public class Kth_SET_BIT {
     public static void main(String[] args){
-        int n = 7, k = 2;
-        if((n & (1 << (k-1))) != 0) System.out.println("YES.");
-        else System.out.println("NO");
+        int n = 5, k = 6;
+//        if((n & (1 << (k-1))) != 0) System.out.println("YES.");
+//        else System.out.println("NO");
+
+
+        // second approach.
+        if((n>>(k-1)&1)==1) System.out.println("Yes.");
+        else System.out.println("No.");
+
     }
 }
