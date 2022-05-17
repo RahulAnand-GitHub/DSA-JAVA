f41bfac29b226236eb6a507cef8acd86bb0dfd63
Rahul_Anand72 <rahul.anand88096@gmail.com>
2022-05-17 21:20:52 +0530

    Lookup Table Method has been added.

--- BitMagic/src/com/company/rahul/COUNT_Set_Bit.java
+++ BitMagic/src/com/company/rahul/COUNT_Set_Bit.java
@@ -1,19 +1,32 @@
 package com.company.rahul;
 
+import java.sql.SQLOutput;
+
 public class COUNT_Set_Bit {
-    public static void main(String[] args){
-        int n = 40;
-        getCount(n);
+    static int[] table = new int[256];
 
 
-    }
-
-    public static void getCount(int n) {
-        int count = 0;
-        while( n > 0){
-            n = n & (n-1);
-            count ++;
+    public static void initialize(){
+        table[0] = 0;
+        for ( int i = 0 ; i < 256 ; i++ ) {
+            table[i] = (i & 1) + table[ i / 2];
         }
-        System.out.println(count);
+    }
+    public static int getCount(int n) {
+//        int count = 0;
+//        while( n > 0){
+//            n = n & (n-1);
+//            count ++;
+//        }
+//        System.out.println(count);
+
+
+//        Lookup Table method
+        return table[n & 0xff] + table[(n >> 8) & 0xff] + table[(n >> 16) & 0xff] + table[n >> 24];
+    }
+    public static void main(String[] args){
+        initialize();
+        int n = 7;
+        System.out.println(getCount(n));
     }
 }
