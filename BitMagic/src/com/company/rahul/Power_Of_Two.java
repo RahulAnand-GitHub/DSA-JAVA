efb3ccb5cce6ed724411797c2ac8a02b04cb42a5
Rahul_Anand72 <rahul.anand88096@gmail.com>
2022-05-18 08:13:56 +0530

    An efficient solution (Brian Kernighan's Algorithm) has been added.

--- BitMagic/src/com/company/rahul/Power_Of_Two.java
+++ BitMagic/src/com/company/rahul/Power_Of_Two.java
@@ -7,13 +7,20 @@ public class Power_Of_Two {
     }
 
     static boolean getPower(int n) {
-        if(n==0)    return false;
-        while(n!=1){
-            if(n % 2 != 0)
-                return false;
-            n/=2;
-        }
+//        if(n==0)    return false;
+//        while(n!=1){
+//            if(n % 2 != 0)
+//                return false;
+//            n/=2;
+//        }
+//
+//        return true;
 
-        return true;
+//        Brian Kernighan's algorithm method.
+
+        if(n==0) return false;
+//        return ((n & (n-1)) == 0);
+        return(n!=0)&&((n & (n-1))==0);
+
     }
 }
