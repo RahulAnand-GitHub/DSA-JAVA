adafb46082dcaf28dd8a42d1ef30f0bc92307245
Rahul_Anand72 <rahul.anand88096@gmail.com>
2022-05-18 20:36:12 +0530

    An efficient solution has been added to the Occurence program.

--- BitMagic/src/com/company/rahul/Occurence.java
+++ BitMagic/src/com/company/rahul/Occurence.java
@@ -9,14 +9,20 @@ public class Occurence {
 
     static void getOccurence(int[] arr) {
 
-        for ( int i = 0 ; i < arr.length  ; i++ ) {
-            int count = 0;
-            for ( int j = 0 ; j < arr.length  ; j++ ) {
-                if (arr[j] == arr[i])
-                    count++;
-            }
-            if (count % 2 != 0)
-                System.out.println(arr[i]);
-        }
+//        for ( int i = 0 ; i < arr.length  ; i++ ) {
+//            int count = 0;
+//            for ( int j = 0 ; j < arr.length  ; j++ ) {
+//                if (arr[j] == arr[i])
+//                    count++;
+//            }
+//            if (count % 2 != 0)
+//                System.out.println(arr[i]);
+//        }
+
+//        XOR Operation
+        int res = 0;
+        for ( int i = 0 ; i < arr.length ; i++ )
+            res = res^arr[i];
+        System.out.println(res);
     }
 }
